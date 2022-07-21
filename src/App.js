b908c0b5d76b4fca5a73221f229f90b34b2b710d
quocvinh0401 <lequocvinh140213@gmail.com>
2022-07-21 08:52:02 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./components/homePage/HomePage";
 import Header from "./components/header/Header";
@@ -15,16 +15,28 @@ function App() {
       {!user ? (
         <Login />
       ) : (
-        <HashRouter>
+        <BrowserRouter>
           <div className="app__content">
             <Header />
             <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/watch" element={<WatchPage />} />
-              <Route path="/marketplace" element={<MarketplacePage />} />
+              <Route
+                path="/facebook-clone-reactjs-demo"
+                exact
+                element={<HomePage />}
+              />
+              <Route
+                path="/facebook-clone-reactjs-demo/watch"
+                exact
+                element={<WatchPage />}
+              />
+              <Route
+                path="/facebook-clone-reactjs-demo/marketplace"
+                exact
+                element={<MarketplacePage />}
+              />
             </Routes>
           </div>
-        </HashRouter>
+        </BrowserRouter>
       )}
     </div>
   );
