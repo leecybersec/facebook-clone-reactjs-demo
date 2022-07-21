127b588983968f3e7dab3d2af6a4ab767e3c4f80
quocvinh0401 <79902360+quocvinh0401@users.noreply.github.com>
2022-07-21 09:14:40 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./components/homePage/HomePage";
 import Header from "./components/header/Header";
@@ -15,28 +15,16 @@ function App() {
       {!user ? (
         <Login />
       ) : (
-        <BrowserRouter>
+        <HashRouter>
           <div className="app__content">
             <Header />
             <Routes>
-              <Route
-                path="/facebook-clone-reactjs-demo/"
-                exact
-                element={<HomePage />}
-              />
-              <Route
-                path="/facebook-clone-reactjs-demo/watch"
-                exact
-                element={<WatchPage />}
-              />
-              <Route
-                path="/facebook-clone-reactjs-demo/marketplace"
-                exact
-                element={<MarketplacePage />}
-              />
+              <Route path="/" exact element={<HomePage />} />
+              <Route path="/watch" element={<WatchPage />} />
+              <Route path="/marketplace" element={<MarketplacePage />} />
             </Routes>
           </div>
-        </BrowserRouter>
+        </HashRouter>
       )}
     </div>
   );
